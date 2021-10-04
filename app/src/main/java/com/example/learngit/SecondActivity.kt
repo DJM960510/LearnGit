739a52329fa986d64f34d7124e1bfa9d6349c4d3
DJM960510 <923231006@qq.com>
2021-10-04 21:09:12 +0800

    做了一些改动

--- app/src/main/java/com/example/learngit/SecondActivity.kt
+++ app/src/main/java/com/example/learngit/SecondActivity.kt
@@ -2,10 +2,12 @@ package com.example.learngit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("SecondActivity","Welcomd to SecondActivity")
         setContentView(R.layout.activity_second)
     }
 }
