e1947776291056f0d1dee70603c0f1e8cd816698
tufuring_deng <tufuring_deng@enable-ets.com>
2021-10-04 19:13:45 +0800

    第二次提交

--- app/src/main/java/com/example/learngit/MainActivity.kt
+++ app/src/main/java/com/example/learngit/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.learngit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var startSecond: TextView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startSecond = findViewById(R.id.start_second)
+        startSecond.setOnClickListener {
+            startActivity(Intent(this, SecondActivity::class.java))
+        }
     }
 }
